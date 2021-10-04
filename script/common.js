13a06e051d2f7edc383b0dbf6baad57444911a8b
okubo-ta <69447462+okubo-ta@users.noreply.github.com>
2021-10-04 16:36:42 +0900

    Update common.js

--- script/common.js
+++ script/common.js
@@ -70,7 +70,6 @@ if(window.innerWidth > window.innerHeight) {	//pc
 else{	//スマホ
 	z_index1.style.opacity = 1;
 	z_index1.style.transform = "scale( "+1+" , "+1+" )";
-	z_index1.style.display = ''
 }
 const log = document.getElementById('log');	//scroll:0
 
